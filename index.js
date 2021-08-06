3cbcc0212e11be0d50dd71a3cc2a87adb2ecb6cf
DarkInk <rstonehouse66@gmail.com>
2021-08-06 00:01:43 -0500

    Show how dumb you are by showing how many times you leaked ur token

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 let config = require('./config.json');
-
+let leaks = '0'
 async function request() {
     axios({
         url: 'https://discord.com/api/v9/applications/' + config.appid + '/bot/reset',
@@ -21,6 +21,9 @@ async function request() {
         messages = messages.filter(x => x.author.id === client.user.id).last();
         messages.edit("Token Has been reset. Please keep a eye out on chats incase this happens again")
         console.log("My New Token is " + client.token)
+        setTimeout(() => {
+            client.user.setActivity("I've had " + leaks + " Tokens Leaked", {type: "WATCHING"})
+        }, 5000);
         }, 3000);
     }).catch(err => {
         console.log(err)
@@ -32,6 +35,7 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 client.on("ready", () => {
     console.log("I'm ready, My token to test with is: " + client.token)
+    client.user.setActivity("I've had " + leaks + " Tokens Leaked", {type: "WATCHING"})
 })
 
 client.on("message", async (message) => {
@@ -39,6 +43,7 @@ client.on("message", async (message) => {
     if(message.content.includes(client.token)) {
         const channel = client.channels.cache.get(config.channelid)
         channel.send('Token was comprised, Reseting token please wait.....')
+        leaks++
         request()
     }
 })
